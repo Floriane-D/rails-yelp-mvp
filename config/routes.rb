d35ee56aa636335dca7bd696eb59626f034ace7e
Floriane Didier <florianedidier@gmail.com>
2019-02-27 12:53:43 -0300

    yelp mvp done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  get "restaurants", to: "restaurants#index"
+  get "restaurants/new", to: "restaurants#new"
+  get "restaurants/:id", to: "restaurants#show"
+  post "restaurants", to: "restaurants#create"
+  get "restaurants/:id", to: "restaurants#show"
+
+  # get "restaurants/:restaurant_id/reviews", to: "reviews#index"
+
   resources :restaurants do
-    resources :reviews, only: [ :new, :create ]
+    resources :reviews, only: [ :index, :new, :create ]
   end
+
+  get "reviews/:id", to: "reviews#show"
 end
